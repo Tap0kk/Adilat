7b80b124d4b00015ee8eae5e724da1e80b14c214
Deman <deman2805@gmail.com>
2024-05-25 19:18:20 +0300

    end adaptive

--- js/main.js
+++ js/main.js
@@ -15,7 +15,63 @@ document.addEventListener("DOMContentLoaded", () => {
             nextEl: '.review-button-next',
             prevEl: '.review-button-prev',
         },
-
+        breakpoints: {
+            // mobile - 320
+            320: {
+                slidesPerView: 1
+            },
+            // tablet - 768
+            768: {
+                slidesPerView: 2
+            },
+        }
     });
 
 });
+
+// -------------------------
+
+const filterBtns = document.querySelectorAll(".works-btn");
+const worksList = document.querySelector(".works-list");
+
+filterBtns.forEach((btn) => {
+    btn.addEventListener("click", () => {
+        filterBtns.forEach((filterBtn) => {
+            filterBtn.classList.remove("is-active");
+        });
+        btn.classList.add("is-active");
+
+        const filterValue = btn.getAttribute("data-filter");
+
+        for (const item of worksList.children) {
+            if (filterValue === "all") {
+                item.classList.remove('hide');
+                item.classList.add('show');
+            } else if (item.classList.contains(filterValue)) {
+                item.classList.remove('hide');
+                item.classList.add('show');
+            } else {
+                item.classList.remove('show');
+                item.classList.add('hide');
+            }
+        }
+    });
+}
+);
+
+// ---------------------------------
+
+(() => {
+  const refs = {
+    openMenuBtn: document.querySelector("[data-menu-open]"),
+    closeMenuBtn: document.querySelector("[data-menu-close]"),
+    menu: document.querySelector("[data-menu]"),
+  };
+
+  refs.openMenuBtn.addEventListener("click", toggleMenu);
+  refs.closeMenuBtn.addEventListener("click", toggleMenu);
+
+  function toggleMenu() {
+    refs.menu.classList.toggle("is-hidden");
+  }
+})();
